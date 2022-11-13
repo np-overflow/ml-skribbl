2891b662b0df72fc5e5b8803067c32a97c4d47cf
Arash <47273556+arashnrim@users.noreply.github.com>
2022-11-14 02:09:53 +0800

    feat: add basic implementation of canvas drawing

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,80 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import { browser } from "$app/environment";
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    if (browser) {
+      const canvas = document.getElementById("board") as HTMLCanvasElement,
+        context = canvas.getContext("2d");
+      var isDrawing = false;
+
+      canvas.height = canvas.offsetHeight;
+      canvas.width = canvas.offsetWidth;
+
+      if (context) {
+        canvas.addEventListener("mousemove", (e) => {
+          if (!isDrawing) return;
+          context.lineWidth = 5;
+          context.lineCap = "round";
+          context.strokeStyle = "black";
+          context.lineTo(e.offsetX, e.offsetY);
+          context.stroke();
+        });
+
+        canvas.addEventListener("mousedown", () => {
+          isDrawing = true;
+          context.beginPath();
+        });
+
+        canvas.addEventListener("mouseup", () => {
+          isDrawing = false;
+        });
+      }
+    }
+  });
+</script>
+
+<main>
+  <section>
+    <canvas id="board" />
+  </section>
+</main>
+
+<div id="background" />
+
+<style>
+  :global(*) {
+    margin: 0;
+  }
+
+  #background,
+  main {
+    height: 100vh;
+    width: 100vw;
+  }
+
+  #background {
+    position: absolute;
+    top: 0;
+    background: linear-gradient(#ff8e2c, #ff5a81);
+  }
+
+  main {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  section {
+    height: 75%;
+    width: 75%;
+    z-index: 1;
+    background-color: white;
+    border-radius: 1rem;
+  }
+
+  canvas {
+    height: 100%;
+    width: 100%;
+  }
+</style>
