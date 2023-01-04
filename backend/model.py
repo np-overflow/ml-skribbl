1a0948aac713c50c81d336882d4620bb736a7547
Arash <47273556+arashnrim@users.noreply.github.com>
2023-01-04 23:46:32 +0800

    chore: clean up

--- backend/model.py
+++ backend/model.py
@@ -38,37 +38,20 @@ class DrawModel:
         image = PIL.Image.open(io.BytesIO(base64.b64decode(
             image_data.removeprefix(b"data:image/png;base64,"))))
 
-        # Create a white rgba background
+        # Creates a new image with a white background using the RGBA mode.
         new_image = Image.new("RGBA", image.size, "WHITE")
-        # Paste the image on the background. Go to the links given below for details.
+        # Pastes the user-drawn image on the background. Go to the links given below for details.
         new_image.paste(image, (0, 0), image)
-        new_image.convert('RGB').save('test.jpg', "JPEG")  # Save as JPEG
 
         image = new_image.convert("L")
         image = image.resize((28, 28))
         image = np.array(image)
 
-        print(type(image))
-
-        # Transform the image
+        # Transforms the image to fit the model.
         image = np.expand_dims(image, axis=0)
         image = np.invert(image)
 
-        # Get Dimensions
-        # print(f"(RAW) Image Input Dimensions: {image_raw.shape}")
-        # print(f"Image Input Dimensions: {image.shape}")
-        # print(f"Model Input Dimensions: {self.model.input_shape}")
-
-        # # image = normalize(list(image))
-        # # plt.imshow(image)
-        # # plt.show()
-        # print(image)
-        # print(image.shape)
-        # # image = image.reshape(1, 28, 28, 1)
-        # # plt.imshow(image)
-        # # plt.show()
-
-        # Make Prediction
+        # Makes a prediction using the model.
         predictions = self.model.predict(image)
         print(f">> Confidence Scores: {predictions}")
         predictions_raw = predictions[0]
