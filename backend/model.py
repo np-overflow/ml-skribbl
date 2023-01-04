8c5a0198bd5fde56d071e19604f5ad8b3cfc5f4d
Addison <S10222525@connect.np.edu.sg>
2023-01-04 17:56:45 +0800

    fix: model predicition - Image fed to model had a background transparency > Model trained on Black background w/ white strokes.

--- backend/model.py
+++ backend/model.py
@@ -1,9 +1,14 @@
 import tensorflow as tf
-import numpy as np
 import matplotlib.pyplot as plt
 import PIL
 import io
 import base64
+
+from keras.models import *
+from PIL import Image
+import numpy as np
+
+
 
 
 def normalize(data):
@@ -25,23 +30,41 @@ class DrawModel:
             3: "Pineapple"
             }
 
-        # Use PIL to convert the image to a grayscale 28x28 image
         image = PIL.Image.open(io.BytesIO(base64.b64decode(
             image_data.removeprefix(b"data:image/png;base64,"))))
-        image = np.asarray(image)
+
+        new_image = Image.new("RGBA", image.size, "WHITE") # Create a white rgba background
+        new_image.paste(image, (0, 0), image)              # Paste the image on the background. Go to the links given below for details.
+        new_image.convert('RGB').save('test.jpg', "JPEG")  # Save as JPEG
+        
+        image = new_image.convert("L")
+        image = image.resize((28, 28))
+        image = np.array(image)
+
+        print(type(image))
 
         # Transform the image 
-        image = image.convert('L')
         image = np.expand_dims(image, axis=0)
         image = np.invert(image)
-        image = normalize(list(image))
-        # plt.imshow(image)
-        # plt.show()
-        print(image)
-        print(image.shape)
-        # image = image.reshape(1, 28, 28, 1)
-        # plt.imshow(image)
-        # plt.show()
+
+        # Get Dimensions
+        # print(f"(RAW) Image Input Dimensions: {image_raw.shape}")
+        # print(f"Image Input Dimensions: {image.shape}")
+        # print(f"Model Input Dimensions: {self.model.input_shape}")
+
+        # # image = normalize(list(image))
+        # # plt.imshow(image)
+        # # plt.show()
+        # print(image)
+        # print(image.shape)
+        # # image = image.reshape(1, 28, 28, 1)
+        # # plt.imshow(image)
+        # # plt.show()
+
+        # Make Prediction
+        predictions = self.model.predict(image)
+        print(f">> Confidence Scores: {predictions}")
         predictions = FRUITS[np.argmax(predictions)]
         print(predictions)
+        print("\n"*10)
         return predictions
