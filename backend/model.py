436789f44efe80b2cc8c3be41cf9f6c096d82e31
Addison <S10222525@connect.np.edu.sg>
2023-01-04 14:31:42 +0800

    fix: image dimensions - transform image to match model dimensions

--- backend/model.py
+++ backend/model.py
@@ -15,12 +15,25 @@ class DrawModel:
     def __init__(self, path) -> None:
         self.path = path
         self.model = tf.keras.models.load_model(self.path)
+        
 
     def predict(self, image_data):
+        FRUITS = {
+            0: "Apple", 
+            1: "Banana", 
+            2: "Grape", 
+            3: "Pineapple"
+            }
+
         # Use PIL to convert the image to a grayscale 28x28 image
         image = PIL.Image.open(io.BytesIO(base64.b64decode(
             image_data.removeprefix(b"data:image/png;base64,"))))
         image = np.asarray(image)
+
+        # Transform the image 
+        image = image.convert('L')
+        image = np.expand_dims(image, axis=0)
+        image = np.invert(image)
         image = normalize(list(image))
         # plt.imshow(image)
         # plt.show()
@@ -29,6 +42,6 @@ class DrawModel:
         # image = image.reshape(1, 28, 28, 1)
         # plt.imshow(image)
         # plt.show()
-        predictions = self.model.predict(image)[0]
+        predictions = FRUITS[np.argmax(predictions)]
         print(predictions)
         return predictions
